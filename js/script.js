2fb6f1e22d44d3329a3230104a62aee14a7476ff
markbulavchyk <marksaitiev@gmail.com>
2025-02-14 19:35:29 -0700

    mod modal window, (open modal after 5s and , open modal when scroll reached end of page

--- js/script.js
+++ js/script.js
@@ -121,12 +121,16 @@ window.addEventListener('DOMContentLoaded', () => {
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
 
+
+    function openModal () { // функция открытия модального окна
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId); // если пользователь сам открыл окно , мы его не вызываем больше , и очищаем интервал
+    }
+
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden'
-        });
+        btn.addEventListener('click', openModal);
     });
 
     function closeModal () { // функция закрытия модального окна , выносим ее отдельно так как мы ее повторяем 2 раза
@@ -148,4 +152,18 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+
+    //  mod modal window 
+
+    const modalTimerId = setTimeout(openModal,5000)
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
 });
